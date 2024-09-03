db268a5109a5e7a7f460db99c142804aeeb38f36
seonhoshin <sunlike0508@waug.com>
2024-09-03 22:29:17 +0900

    연산자

---
+++ src/main/kotlin/operator/ComparisonOperator.kt
@@ -0,0 +1,24 @@
+package operator
+
+import kotlin.random.Random
+
+class ComparisonOperator {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args : Array<String>) {
+            val a = Random.nextInt(10);
+            val b = Random.nextInt(10);
+            val c = Random.nextInt(10);
+
+            println("a : $a");
+            println("b : $b");
+
+            println(a > b);
+            println(a != b);
+
+            println("a: $a, b : $b, c : $c")
+        }
+    }
+}
