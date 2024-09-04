82c8d63f51a8b8c7f5e57ec613e40c42675d90f5
seonhoshin <sunlike0508@gmail.com>
2024-09-04 22:58:26 +0900

    함수

---
+++ src/main/kotlin/function/FunctionSample.kt
@@ -0,0 +1,36 @@
+package function
+
+fun main(args : Array<String>) {
+    val sum = sum1(10, 20)
+
+    println(sum)
+
+    println(sum2(20, 30))
+
+    noReturnFunction("hello")
+
+    defaultParameter("no value")
+    defaultParameter()
+
+    namedArgument("I", "am", "seonho")
+    namedArgument(a= "I", c="seonho", b="am")
+}
+
+
+fun sum1(a: Int, b: Int): Int {
+    return a + b;
+}
+
+fun sum2(a:Int, b:Int) = a+b
+
+fun noReturnFunction(a:String) {
+    println("called function : $a")
+}
+
+fun defaultParameter(a:String ="default value") {
+    println("defaultParameter : $a")
+}
+
+fun namedArgument(a:String, b:String, c:String) {
+    println("$a , $b , $c")
+}
